cb4e44d942bd72fe463901196800c31d033e99a4
Dharmendra Korada <v-dhkor@microsoft.com>
2016-04-19 19:46:02 +0530

    Infrastructure changes

--- WebPortal/Properties/AssemblyInfo.cs
+++ WebPortal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Admin")]
+[assembly: AssemblyTitle("WebPortal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Admin")]
+[assembly: AssemblyProduct("WebPortal")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dcd134f4-2bd7-4f04-8f41-29e3098b0bce")]
+[assembly: Guid("0fca2464-59af-4e90-bbeb-2c30e1a789db")]
 
 // Version information for an assembly consists of the following four values:
 //
